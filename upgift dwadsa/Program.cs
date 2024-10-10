8d1feba70c4c6491fd6614bb17c70119d88dde18
Victor Johansson <vijo07001@utb.vaxjo.se>
2024-10-10 13:03:35 +0200

    Add project files.

---
+++ upgift dwadsa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Upgift_2_4
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket känar allihopa en efter en Bara siffror");
+            string person1 = Console.ReadLine();
+            int P1 = int .Parse(person1)
+            string person2 = Console.ReadLine();
+            int P2 = int .Parse(person2)
+            string person3 = Console.ReadLine();
+            int P3 = int .Parse(person3);
+            int Sum = (P1 + P2 + P3);
+            int medel = ( Sum / 3);
+            Console.WriteLine("medel lönen i företaget är " + medel);
+        }
+     }
+}
